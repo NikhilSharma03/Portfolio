16f73d95ef317284f60c3eeca2cb54f14b4dc4c8
Nikhil Sharma <nikhilsharmamusic2000@gmail.com>
2022-01-15 18:13:05 +0530

    added tech logos

--- src/shared/ImgSrc.tsx
+++ src/shared/ImgSrc.tsx
@@ -1,3 +1,47 @@
 import HeroBanner from "./../assets/hero_logo.png";
+import cicd from "./../assets/technology/cicd.png";
+import docker from "./../assets/technology/docker.png";
+import expressjs from "./../assets/technology/expressjs.png";
+import gin from "./../assets/technology/gin.png";
+import git from "./../assets/technology/git.png";
+import github from "./../assets/technology/github.png";
+import golang from "./../assets/technology/golang.png";
+import grpc from "./../assets/technology/grpc.png";
+import javascript from "./../assets/technology/javascript.png";
+import kubernetes from "./../assets/technology/kubernetes.png";
+import mongodb from "./../assets/technology/mongodb.png";
+import mysql from "./../assets/technology/mysql.png";
+import nodejs from "./../assets/technology/nodejs.png";
+import python from "./../assets/technology/python.png";
+import react from "./../assets/technology/react.png";
+import redis from "./../assets/technology/redis.png";
+import redux from "./../assets/technology/redux.png";
+import rust from "./../assets/technology/rust.png";
+import slack from "./../assets/technology/slack.png";
+import solidity from "./../assets/technology/solidity.png";
+import typescript from "./../assets/technology/typescript.png";
 
-export { HeroBanner };
+export {
+  HeroBanner,
+  cicd,
+  docker,
+  expressjs,
+  gin,
+  git,
+  github,
+  golang,
+  grpc,
+  javascript,
+  kubernetes,
+  mongodb,
+  mysql,
+  nodejs,
+  python,
+  react,
+  redis,
+  redux,
+  rust,
+  slack,
+  solidity,
+  typescript,
+};
